3d26942473ae93e00c18fedf99877389da0963ff
Brendis21 <106267531+Brendis21@users.noreply.github.com>
2023-02-06 22:01:40 -0600

    initial commit

---
+++ 8SIS1AWeb_MILLAN_ESQUIVEL_BRENDA_XIMENA/Inicio.html/app.js
@@ -0,0 +1,11 @@
+function ir(){
+
+    var c=2805;
+    var u="Brenda";
+    if(document.form.password.value==c && document.form.login.value==u){
+    alert ("Bienvenido, sus datos son correctos");}
+    else{
+    alert("Los datos ingresados son incorrectos, intenta de nuevo.");
+    }
+    
+    }
